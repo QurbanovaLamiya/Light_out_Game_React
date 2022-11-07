be69f884c5e82abbf7373353f3b6d2bdd593e8ee
QurbanovaLamiya <lamiyaqurbanova121@gmail.com>
2022-11-07 23:46:42 +0400

    Update

---
+++ src/components/Cell/index.js
@@ -0,0 +1,13 @@
+import { Component, Fragment } from "react";
+
+class Cell extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Cell</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default Cell;
